ce1d8746281620739edcadb5f730f66bbe6b66e2
William Aguas <148153196+WILLIAMAGUAS@users.noreply.github.com>
2025-12-04 03:59:30 +0000

    primera parte

--- src/models.py
+++ src/models.py
@@ -1,15 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
-
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    favorit_people: Mapped[List["FavoritPeople"]] = relationship()
+    favorit_planet: Mapped[List["FavoritPlaneta"]] = relationship()
 
     def serialize(self):
         return {
@@ -17,3 +19,54 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class People(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    age: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    height: Mapped[str] = mapped_column(nullable=False)
+    weight: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    favorit_people: Mapped[List["FavoritPeople"]] = relationship()
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+
+class FavoritPeople(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    id_user: Mapped[int] = mapped_column(
+        db.ForeignKey("user.id"), primary_key=True)
+    id_people: Mapped[int] = mapped_column(
+        db.ForeignKey("people.id"), primary_key=True)
+
+
+class Planeta(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    diametro: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    gravedad: Mapped[str] = mapped_column(nullable=False)
+    clima: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    name: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    favorit_planet: Mapped[List["FavoritPlaneta"]] = relationship()
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+
+class FavoritPlaneta(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    id_user: Mapped[int] = mapped_column(
+        db.ForeignKey("user.id"), primary_key=True)
+    id_planeta: Mapped[int] = mapped_column(
+        db.ForeignKey("planeta.id"), primary_key=True)
